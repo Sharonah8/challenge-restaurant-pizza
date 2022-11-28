85106e0e439e3c605c83ff5670ef262f177663e4
Sharonah8 <nyamongosharon88@gmail.com>
2022-11-28 12:28:29 +0300

    Add a rescue_from for the errors

--- app/controllers/restaurantpizzas_controller.rb
+++ app/controllers/restaurantpizzas_controller.rb
@@ -1,2 +1,8 @@
 class RestaurantpizzasController < ApplicationController
+rescue_from ActiveRecord::RecordInvalide, with: :unprocessable_entity_method
+
+private
+def unprocessable_entity_method(exception)
+    render json: { errors: errors.record.errors}, status: :unprocessable_entity_method
+end
 end
