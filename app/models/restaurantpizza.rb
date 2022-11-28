5e7c51b3edffaa16e0a5f4d19e4165a0c109664c
Sharonah8 <nyamongosharon88@gmail.com>
2022-11-28 12:23:26 +0300

    Set validations for prices less than 1 and greater than 30

--- app/models/restaurantpizza.rb
+++ app/models/restaurantpizza.rb
@@ -1,4 +1,12 @@
 class Restaurantpizza < ApplicationRecord
     belongs_to :restaurant 
     belongs_to :pizza 
+
+    validates :price, presence: true
+    validate :price_range
+
+    def price_range
+        if (price < 1 || price > 30)
+            errors.add(:price, "the price must be between 0 and 30")
+    end
 end
