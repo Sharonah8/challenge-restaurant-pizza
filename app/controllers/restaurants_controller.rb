b5c6893837d3417b34309599fc5694656da0cd7f
Sharonah8 <nyamongosharon88@gmail.com>
2022-11-28 15:08:14 +0300

    Specify index and show actions for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
     def index
         render json: Restaurant.all
     end
@@ -6,5 +7,11 @@ class RestaurantsController < ApplicationController
     def show
         restaurant = Restaurant.find(params[:id])
         render json: restaurant
+    end
+
+    private
+
+    def record_not_found
+        render json: { error: "Restaurant not found"}, status: :not_found 
     end
 end
