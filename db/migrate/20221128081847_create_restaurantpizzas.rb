13312bc82516d3900bd64296ae4cf009c2ed78dd
Sharonah8 <nyamongosharon88@gmail.com>
2022-11-28 11:19:19 +0300

    Generate resources for pizza, restaurant, and restaurantpizza models

---
+++ db/migrate/20221128081847_create_restaurantpizzas.rb
@@ -0,0 +1,10 @@
+class CreateRestaurantpizzas < ActiveRecord::Migration[7.0]
+  def change
+    create_table :restaurantpizzas do |t|
+      t.integer :pizza_id
+      t.integer :restaurant_id
+
+      t.timestamps
+    end
+  end
+end
